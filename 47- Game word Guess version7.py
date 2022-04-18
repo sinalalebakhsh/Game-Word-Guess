7d16deb36d62cd63ec12793918630fcf6321b6c4
sina lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2022-04-18 12:00:14 +0430

    نسخۀ7 گرفتن ورودی از کاربر برای تعداد دفعات بازی
    این حرکت خیلی جالب کرده این بازی رو که خود کاربر انتخاب کنه چه تعداد می خواد بازی کنه.
    نسخۀ7 گرفتن ورودی از کاربر برای تعداد دفعات بازی
    How many rounds do you play?
    This move has made this game very interesting for the user to choose how many he wants to play.
    Version 7 Get input from the user for the number of times the game

---
+++ 47- Game word Guess version7.py
@@ -0,0 +1,56 @@
+import os
+os.system('cls')
+user_input = 0
+
+def number_rounds(user_input):    
+    user_input = int(input('How many rounds do you play? '))
+    return user_input
+
+def dividing_line():
+    print('-'*20)
+
+def get_input():
+    while True:
+        user_input = input('Enter word: ')
+        if user_input.isalpha():
+            return user_input
+        dividing_line()
+        print('Wrong word. Try again.')
+
+def select_in_list(user_input, words):
+    while user_input.lower() not in words:
+        dividing_line()
+        print('Wrong word. Try again in list')
+        user_input = get_input()
+    return user_input.lower()
+
+def print_game_intro():
+    dividing_line()
+    print('Wellcome')
+    print('word for guess:', list_word)
+    dividing_line()
+
+def run_game(number_rounds, words):
+    print_game_intro()
+    print(f'number of guess = {number_rounds}')
+    correct_word = words[1]
+    for s in range(number_rounds):
+        user_input = get_input()
+        select_in_list(user_input, words)
+        if user_input == correct_word:
+            dividing_line()
+            print('YOU WON.')
+            dividing_line()
+            return
+        else:
+            print('Guessed wrong')
+            print(f'Rounds left: {number_rounds-1-s}')
+    dividing_line()
+    print('You Lost')
+    dividing_line()
+
+list_word = ['sina', 'mahsa', 'lale', 'gilan', 'kimia', 'gandom', 'ommid', 'mina', 'lina', 'dina']
+
+run_game(number_rounds(user_input), list_word)
+
+
